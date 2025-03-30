d6399553da5e93f589ee7513e74e21f15788eb98
j842 <j@842.be>
2025-03-30 22:38:17 +1300

    full size pic option

--- www/script.js
+++ www/script.js
@@ -369,6 +369,12 @@ function showImagePopup(image, isNavigating = false) {
             existingNavigation.remove();
         }
         
+        // Remove any existing fullres link
+        const existingFullresLink = document.querySelector('.fullres-link');
+        if (existingFullresLink) {
+            existingFullresLink.remove();
+        }
+        
         // Set the popup content
         popupImage.classList.add('loading');
         
@@ -383,6 +389,30 @@ function showImagePopup(image, isNavigating = false) {
         
         // Show the popup
         popup.style.display = 'block';
+        
+        // Add fullres link to open original image in new tab
+        const imageContainer = document.querySelector('.image-container');
+        const fullresLink = document.createElement('a');
+        fullresLink.className = 'fullres-link';
+        fullresLink.href = `/data/${currentWalkId}/images/${image.imageName}`;
+        fullresLink.target = '_blank';
+        fullresLink.title = 'Open full resolution image in new tab';
+        fullresLink.setAttribute('aria-label', 'Open full resolution image');
+        
+        // Add SVG icon for fullscreen/open in new tab
+        fullresLink.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M19 19H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/>
+            </svg>
+        `;
+        
+        // Prevent propagation to avoid closing the popup when clicking the link
+        fullresLink.addEventListener('click', function(e) {
+            e.stopPropagation();
+        });
+        
+        // Add to container
+        imageContainer.appendChild(fullresLink);
         
         // Create mini map with the trail path and current location
         if (map && settings.trailPath) {
